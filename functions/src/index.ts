2dad21c2523f944a9446f962fcb5ecc01184ce2d
MaxiBal <maxibal@baltel.com>
2022-12-13 20:57:08 -0500

    role based auth

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,18 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import * as cors from "cors";
+import * as bodyParser from "body-parser";
+
+import {routesConfig} from "./users/routes-config";
+
+admin.initializeApp();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({origin: true}));
+routesConfig(app);
+
+export const api = functions.https.onRequest(app);
+
